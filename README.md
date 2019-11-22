f60094883739f3377593c4487a168e3662d3e59a
roadricefield <sciencemath1455@gmail.com>
2019-11-22 14:21:12 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Functions for programing contest
+
+## pri_fac(*long long*, *string*)  
+This fanction performs prime factorization. The calculation cost is O(sqrt(*N*)).  
+This fanction has two modes.
+
+- all  
+Returns all prime factors of given long long values.
+```cpp
+vector<long long> out = pri_fac(30000LL, all);
+
+for(int i=0;i<out.size();i++) cout << out[i] << endl;
+
+//output
+2
+2
+2
+2
+3
+5
+5
+```
+
+- unique  
+Returns unique prime factors of given long long  values.
+```cpp
+vector<long long> out = pri_fac(30000LL, unique);
+
+for(int i=0;i<out.size();i++) cout << out[i] << endl;
+
+///output
+2
+3
+5
+```
+
+
---
+++ pri_fac.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<long long> pri_fac(long long N, string mode){
+    if(mode == "unique"){
+        vector<long long> yakusu;
+        long long now = 0; 
+        long long a = 2;
+        while(N >= a*a){
+            if(N%a == 0){
+                if (a != now){
+                    yakusu.push_back(a);
+                    now = a;
+                }
+                N /= a;
+            }else{
+                a++;
+            }
+        } 
+        if(N != now & N != 1) yakusu.push_back(N);
+        return yakusu;
+    }else if(mode == "all"){
+        vector<long long> yakusu;
+        long long now = 0; 
+        long long a = 2;
+            while(N >= a*a){
+                if(N%a == 0){
+                    yakusu.push_back(a);
+                    now = a;
+                    N /= a;
+                }else{
+                    a++;
+                }
+            } 
+        if(N != now & N != 1) yakusu.push_back(N);
+        return yakusu;
+    }
+}
+
+int main(){
+    long long a;
+    string S;
+    cin >> a;
+    cin >> S;
+
+    vector<long long> test;
+
+    test = pri_fac(a,S);
+
+    for(int i=0;i<test.size();i++){
+        cout << test[i] << endl;
+    }
+
+    return 0;
+}
